4de43392e6dc3415e510cba7c2482641591d4840
nchronas <nchronas@users.noreply.github.com>
2024-04-27 15:52:49 +0200

    test oled screen as5311.cpp

--- as5311.cpp
+++ as5311.cpp
@@ -6,8 +6,6 @@
 
             Quad A --------------------  D2
             Quad B --------------------  D3
-
-            1.3" 128x64 SSD1306 OLED Display on I2C bus         
                     
 //============================================================================================
 */
@@ -16,7 +14,7 @@
 
 #include <U8glib.h>
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);
 
 char Reading[9];
 
@@ -24,8 +22,14 @@ Encoder Encoder1(2, 3);
 
 void setup() {
   Serial.begin(9600);
-  u8g.begin(); 
-  u8g.setFont(u8g_font_p01type);
+}
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  u8g.setPrintPos(0, 20); 
+  // call procedure from base class, http://arduino.cc/en/Serial/Print
+  u8g.print(Reading);
 }
 
 void loop() {
@@ -36,10 +40,9 @@ void loop() {
     // using the dtostrf function below since this sets the number of decimal places for either metric or imperial readings
     dtostrf(mm,7,3,Reading);
 
-    u8g.drawStr(0,28,"Y");
-    u8g.drawStr( 24, 28, Reading);
-    u8g.drawStr( 110, 28, "mm");
 
+    draw();
+   
     Serial.println(mm);
-    delay(50);    
+    delay(500);    
 }
