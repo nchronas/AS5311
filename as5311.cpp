fae22a982693860c976031ab51a92f79ea7b05a8
nchronas <nchronas@users.noreply.github.com>
2024-04-27 15:09:20 +0200

    testing oled display

--- as5311.cpp
+++ as5311.cpp
@@ -6,24 +6,39 @@
 
             Quad A --------------------  D2
             Quad B --------------------  D3
+
+            1.3" 128x64 SSD1306 OLED Display on I2C bus         
                     
 //============================================================================================
 */
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
 
-long position = 0;
+#include <U8glib.h>
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);
+
+char Reading[9];
 
 Encoder Encoder1(2, 3);
 
 void setup() {
-   Serial.begin(9600);
+  Serial.begin(9600);
+  u8g.begin(); 
+  u8g.setFont(u8g_font_p01type);
 }
 
 void loop() {
-   
-    position = Encoder1.read();
+ 
+    long position = Encoder1.read();
     float mm = (float)position/512.000; 
+
+    // using the dtostrf function below since this sets the number of decimal places for either metric or imperial readings
+    dtostrf(mm,7,3,Reading);
+
+    u8g.drawStr(0,28,"Y");
+    u8g.drawStr( 24, 28, Reading);
+    u8g.drawStr( 110, 28, "mm");
 
     Serial.println(mm);
     delay(50);    
